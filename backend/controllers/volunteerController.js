18af566e151734c3c43d98a772e62861bffcfcc0
Shivam kumar <shivam68338@gmail.com>
2026-02-13 02:15:54 +0530

    Update volunteerController.js

--- backend/controllers/volunteerController.js
+++ backend/controllers/volunteerController.js
@@ -55,12 +55,15 @@ export const registerVolunteer = async (req, res) => {
 
     // 🔹 EMAIL TRANSPORTER
     const transporter = nodemailer.createTransport({
-  service: "smtp.gmail.com",
-  port: 587, 
-  secure: false, 
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
