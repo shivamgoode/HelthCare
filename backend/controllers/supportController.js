5af9e395d0d253eb84069c71c1642fc92eea3f30
Shivam kumar <shivam68338@gmail.com>
2026-02-13 01:54:59 +0530

    Update supportController.js

--- backend/controllers/supportController.js
+++ backend/controllers/supportController.js
@@ -46,12 +46,15 @@ export const submitSupport = async (req, res) => {
 
     // 🔹 EMAIL TRANSPORTER
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
 
     // 🔹 EMAIL TEMPLATE
     const mailOptions = {
